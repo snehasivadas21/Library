c6085e9e37f01421e6e55bdb46aa2e4abe84bd0f
snehasivadas21 <snehasivadas2002@gmail.com>
2026-09-17 21:00:28 +0530

    add filtering,searching,ordering,pagination

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets,filters
+
 from .models import Book
 from .serializers import BookSerializer
-
+from .pagination import BookPagination
 # Create your views here.
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = BookPagination
+
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+
+    filter_fields = ["category","is_available"]
+
+    search_fields = ["title","author"]
+
+    ordering_fields = ["published_date"]
+
+    ordering = ["published_date"]
